15c295171f7d44e99afe4e9bc6f44e8695aa409c
Gabriel <gabrielleong917@gmail.com>
2024-02-19 12:06:37 -0800

    finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+
+import classes from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>This is a test text</main>;
+  return (
+    <main className={classes.main}>
+      <div className={classes.container}>
+        <div className={classes.box}>
+          <div className={classes.title}>
+            <h1>Spirit Island Game Tracker</h1>
+          </div>
+
+          <div className={classes.about}>
+            Record all your Spirit Island game sessions or explore the different
+            Spirits and Adversaries currently in the game.
+          </div>
+
+          <div className={classes.register}>
+            <Link href="/login" className={classes.button}>
+              Register / Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
