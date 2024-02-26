27ec6f08f67cf274627cc70c2dbaaa3a6f485bec
Gabriel <gabrielleong917@gmail.com>
2024-02-26 15:34:20 -0800

    finished initial spirit pages

--- src/pages/spirits/[pathname].tsx
+++ src/pages/spirits/[pathname].tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
 import type { GetStaticPaths, GetStaticProps } from "next";
+
+import classes from "./page.module.css";
 
 interface Spirit {
   id: number;
@@ -17,7 +20,63 @@ const SpiritDetails = (props: Spirit) => {
         </title>
         <meta name="description" content={props.name} />
       </Head>
-      <div>{props.name}</div>
+
+      <div className={classes.container}>
+        <div className={classes.wrapper}>
+          <div className={classes.header}>
+            <h1>{props.name}</h1>
+          </div>
+
+          <div className={classes.content}>
+            <section className={classes.section__intro}>
+              <div className={classes.imageWrapper}>
+                <div className={classes.imageContainer}>
+                  <Image
+                    className={classes.spiritImage}
+                    src={props.image}
+                    alt={props.name}
+                    height={500}
+                    width={500}
+                  />
+                </div>
+              </div>
+
+              {/* TODO: Add text to database about playstyle, setup, and complexity */}
+              <div className={classes.textContainer}>
+                <div className={classes.playstyle_subsection}>
+                  <h2>PLAY STYLE</h2>
+                  <p>
+                    Virtually all offense to start with: without a more
+                    defensive teammate, Blight may become a problem. Excellent
+                    at destroying buildings, less good at containing Explorers.
+                    Using Thundering Destruction tends to be a burst affair: a
+                    turn or two of position and build up Energy, followed by a
+                    really big turn. Starting Powers are extremely focused on
+                    Air and Fire: good for Thundering Destruction, bad for Major
+                    Power versatility.
+                  </p>
+                </div>
+
+                <div className={classes.subInfo}>
+                  <div
+                    className={`${classes.intro__subsection} ${classes.marginRight}`}
+                  >
+                    <h2>SETUP</h2>
+                    <p>
+                      Put 2 Presence on your starting board in the
+                      highest-numbered Sands.
+                    </p>
+                  </div>
+                  <div className={classes.intro__subsection}>
+                    <h2>COMPLEXITY</h2>
+                    <p>LOW</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
